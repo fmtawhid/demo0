35063b202652bb458e65d59149b46c547447502b
Tawhid Islam <79523960+fmtawhid@users.noreply.github.com>
2023-11-02 20:00:35 +0600

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def page(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse("<h1>Hello World I am Tawhid from Bangladesh</h1>")
 
